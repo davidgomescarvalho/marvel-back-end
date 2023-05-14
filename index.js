c77ca54f433f655fe441e72b81942492ebc56d8d
David Gomes <davidgomesk8@gmail.com>
2023-05-14 18:51:54 +0200

    update

--- index.js
+++ index.js
@@ -24,6 +24,34 @@ app.get("/characters", async (req, res) => {
   }
 });
 
+app.get("/character/:characterId", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/character/${req.params.characterId}?apiKey=${process.env.API_KEY}`
+    );
+    const characterId = response.data;
+    res.status(200).json(characterId);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.get("/comics", async (req, res) => {
+  try {
+    const title = req.query.title || "";
+    const limit = req.query.limit || "100";
+    const skip = req.query.skip || "0";
+
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.API_KEY}&title=${title}&skip=${skip}&limit=${limit}`
+    );
+    const comics = response.data;
+    res.status(200).json(comics);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
 app.all("*", (req, res) => {
   res.status(404).json({ message: "cette Route n'existe pas" });
 });
